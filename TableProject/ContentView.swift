3dd7057f77c157ea549651d69da3b8680ce40de4
rodrigoramos <rodrigo.alberto.ramos.q@gmail.com>
2022-09-01 09:21:38 -0500

    Initial Commit

---
+++ TableProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TableProject
+//
+//  Created by Rodrigo Ramos on 1/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TableProject/TableProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableProjectApp.swift
+//  TableProject
+//
+//  Created by Rodrigo Ramos on 1/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TableProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
